36bb3377a5376fc9d45d3ad608195599cf8df05c
Daan Ketelaars <daanketelaars@gmail.com>
2022-06-02 13:38:46 +0200

    add component folder

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div className="grid">
-      <section className="Hero">
-        <h1>Hello world</h1>
-      </section>
-    </div>
-  );
+  return <div className="grid"></div>;
 }
