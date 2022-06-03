287acbb58c21ff2bb6d684713b7222357ac84777
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2022-06-03 15:24:29 +0200

    header images added

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,6 +3,11 @@ import Image from "next/image";
 // Images
 import Dummy from "../../assets/ilojo-bar-1946.jpg";
 import Paper from "../../assets/ilojo-bar.jpg";
+import architectTop from "../../public/images/home/home-photo-01.png"
+import architectSide from "../../public/images/home/home-photo-02.png"
+import oldBuilding from "../../public/images/home/home-photo-03.png"
+import newBuilding from "../../public/images/home/home-photo-04.png"
+
 
 export default function Header() {
   return (
@@ -10,13 +15,13 @@ export default function Header() {
       {/*Top images*/}
       <div>
         <Image
-          alt="The guitarist in the concert."
-          src={Dummy}
+          alt="Ilojo bar top view"
+          src={architectTop}
           layout="responsive"
         />
         <Image
-          alt="The guitarist in the concert."
-          src={Dummy}
+          alt="Ilojo bar side view"
+          src={architectSide}
           layout="responsive"
         />
       </div>
@@ -26,13 +31,13 @@ export default function Header() {
       {/*Bottom images*/}
       <div>
         <Image
-          alt="The guitarist in the concert."
-          src={Dummy}
+          alt="The ilojo in 20th century"
+          src={newBuilding}
           layout="responsive"
         />
         <Image
-          alt="The guitarist in the concert."
-          src={Dummy}
+          alt="The ilojo bar in 19th century"
+          src={oldBuilding}
           layout="responsive"
         />
       </div>
