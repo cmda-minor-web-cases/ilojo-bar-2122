efd1b072097e46822099c348eae93143519631d7
Daan Ketelaars <daanketelaars@gmail.com>
2022-06-14 14:31:48 +0200

    data addd

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,20 +1,37 @@
-import { gql } from "@apollo/client";
-import client from "../../apolloClient";
+import { GraphQLClient, gql } from "graphql-request";
+const graphcms = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
+let mains;
+let blocks;
 
-export async function getHeaderContent() {
-  const { data } = await client.query({
-    query: gql`
-      query {
-        headers {
-          heading
+export async function getAllStories() {
+  const query = gql`
+    {
+      mains {
+        blocks {
+          ... on Story {
+            id
+            bodytext01 {
+              text
+            }
+            bodytext02 {
+              text
+            }
+            images {
+              id
+              url
+            }
+            title
+            subtitle
+          }
         }
       }
-    `,
+    }
+  `;
+
+  const results = await graphcms.request(query);
+  mains = results.mains;
+  mains.forEach((item) => {
+    blocks = item.blocks;
   });
-  const { headers } = data;
-  return {
-    props: {
-      headers,
-    },
-  };
+  return blocks;
 }
