a7c71c2f55e3136f75215e87aaf83cfc1009c11b
Daan Ketelaars <daanketelaars@gmail.com>
2022-06-15 14:20:58 +0200

    update

--- components/main/Main.js
+++ components/main/Main.js
@@ -10,30 +10,28 @@ export default function Main() {
 
   return (
     <main>
-      <div>
-        {context.map((ctx, i) => (
-          <article key={i}>
-            <h4>{ctx.subtitle}</h4>
-            <h2>{ctx.title}</h2>
-            <p>{ctx.bodytext01.text}</p>
-            <p>{ctx.bodytext02.text}</p>
-            {ctx.images.map((image) => {
-              return (
-                <div key={image.id}>
-                  <Image
-                    src={image.url}
-                    alt="foto"
-                    layout="responsive"
-                    width="100%"
-                    height="100%"
-                    objectFit="contain"
-                  />
-                </div>
-              );
-            })}
-          </article>
-        ))}
-      </div>
+      {context.map((ctx, i) => (
+        <article key={i}>
+          <h4>{ctx.subtitle}</h4>
+          <h2>{ctx.title}</h2>
+          <p>{ctx.bodytext01.text}</p>
+          <p>{ctx.bodytext02.text}</p>
+          {ctx.images.map((image) => {
+            return (
+              <div key={image.id}>
+                <Image
+                  src={image.url}
+                  alt="foto"
+                  layout="responsive"
+                  width="100%"
+                  height="100%"
+                  objectFit="contain"
+                />
+              </div>
+            );
+          })}
+        </article>
+      ))}
       {/* Intro */}
       {/* Loop door stories */}
     </main>
