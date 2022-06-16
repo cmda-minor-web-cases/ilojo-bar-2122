ed7155950a4cb6975fd8c6302940e3b34fe1813b
Sophie van der Burg <sophievdburg9@gmail.com>
2022-06-16 11:39:50 +0200

    right order of the story's

--- router/routes/timelineRoute.js
+++ router/routes/timelineRoute.js
@@ -2,6 +2,9 @@ import { client } from '../../config/prismicConfig.js'
 
 export const renderTimeline = async (req, res) =>{
   let allStories = await client.getAllByType('story')
+    
+  allStories.sort((a, b) => a.data.numberid - b.data.numberid)
+
   res.render('timeline', 
   { allStories, 
     css : "/styles/timeline.css" 
