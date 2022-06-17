b47ae42f803c97d612294a6856ca1c389ffaf7bc
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2022-06-17 15:59:41 +0200

    Conditional rendering added

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -5,13 +5,12 @@ import styles from "../../styles/footer/Footer.module.scss";
 
 // Images
 import Dummy from "../../assets/ilojo-bar-1946.jpg";
-import Paper from "../../assets/ilojo-bar.jpg";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h2>
-        The ilojo bar<span>In {`it's`} full glory</span>
+        The ilojo bar in it&#39;s full glory
       </h2>
       {/*3D Ilojo Bar*/}
       <Image
