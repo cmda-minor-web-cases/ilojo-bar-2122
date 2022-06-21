500a897317d0ebc34ee83934b4f044dd189df9aa
Sophie van der Burg <sophievdburg9@gmail.com>
2022-06-21 20:00:59 +0200

    sending mail with image works

--- router/routes/multer.js
+++ router/routes/multer.js
@@ -3,18 +3,16 @@ import express from 'express'
 
 const app = express()
    
-export const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/images/uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.filename)
-  	}
-})
 
-export const upload = multer({ storage: storage })
+export const upload = multer({ dest: "public/images/uploads/" });
 
 export function multerUploads(req, res){
     console.log(req.body)
-    console.log(req.files)
+    console.log(req.files[0])
+
+    const images = req.files
+
+    const attachments = images.map((image)=>{
+        return { filename: image.filename, path: image.destination + image.filename };
+    });
 }
--- router/routes/postSendStoryRoute.js
+++ router/routes/postSendStoryRoute.js
@@ -4,7 +4,7 @@ import multer from 'multer'
 import * as dotenv from "dotenv"
 import 'dotenv/config'
 
-import { upload, storage, multerUploads } from './multer.js'
+import { upload,  multerUploads } from './multer.js'
 
 const {
     MAIL_PW
@@ -15,26 +15,30 @@ export const postSendStory = async (req, res, next) =>{
     // uploaded files to images/uploads
     multerUploads(req, res)
     
+    const images = req.files
+
+    const attachments = images.map((image)=>{
+        return { filename: image.filename, path: image.destination + image.filename };
+    })
+        
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        secureConnection: false,
+      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      secureConnection: false,
         auth: {
           user: 'ilojobar@gmail.com',
           pass: MAIL_PW
         }
-      })
-    
+    })
+
     let info = {
-        from: req.body.mail, 
-        to: 'ilojobar@gmail.com',
-        subject: req.body.title, 
-        html: "<h4>Name:</h4>" + req.body.name + "<h4>E-mail:</h4>" + req.body.mail + "<h4>The title:</h4> " + req.body.title + "<h4>The story:</h4>" + req.body.story,
-      //   attachments: [
-      //     { filename: '1.png', path: './public/images/1.png' }
-      //  ]
+      from: req.body.mail, 
+      to: 'ilojobar@gmail.com',
+      subject: req.body.title, 
+      html: "<h4>Name:</h4>" + req.body.name + "<h4>E-mail:</h4>" + req.body.mail + "<h4>The title:</h4> " + req.body.title + "<h4>The story:</h4>" + req.body.story,
+        attachments: attachments
     };
     
     transporter.sendMail(info, function(error){
