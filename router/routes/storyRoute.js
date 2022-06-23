0de596070a2c419b1ae626897c4ab0c532b44348
juliandecloe <julian.de.cloe@hva.nl>
2022-06-23 13:45:10 +0200

    Story styling and performance enhancement

--- router/routes/storyRoute.js
+++ router/routes/storyRoute.js
@@ -6,14 +6,15 @@ stories = stories.map(story => {
     return {
         "uid": story.uid,
         "title": story.data.title[0].text,
-        "text": story.data.text
+        "text": story.data.text,
+        "images": [story.data.image1, story.data.image2, story.data.image3, story.data.image4, story.data.image5, story.data.image6, story.data.image7]
     }
 });
  
 stories = stories.sort((a, b) => a.uid - b.uid);
 
 export const renderStory = (req, res) =>{
-    let singleStory = stories.filter(story => story.title.split(' ').join('_').toLowerCase() === req.params.title)
+    let singleStory = stories.find(story => story.title.split(' ').join('_').toLowerCase() === req.params.title);
     res.render('story', {
         singleStory
     });
